3e908bf968cff3b33d6d8bcdfb221d735d025db7
revacollege <channaverkergota7.in>
2026-06-26 15:53:47 +0530

    Added Week 1 Exercise 3 and Exercise 4

---
+++ week1/Exercise3_EcommerceSearchFunction/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+     public static void main(String[] args) {
+        Product[] products = {
+                new Product(101, "Laptop", "Electronics"),
+                new Product(102, "Phone", "Electronics"),
+                new Product(103, "Shoes", "Fashion"),
+                new Product(104, "Watch", "Accessories"),
+                new Product(105, "Book", "Education")
+        };
+        System.out.println("Linear Search:");
+        Product p1 = SearchEngine.linearSearch(products, 103);
+        if (p1 != null)
+            p1.display();
+        else
+            System.out.println("Product Not Found");
+        System.out.println();
+        System.out.println("Binary Search:");
+        Product p2 = SearchEngine.binarySearch(products, 104);
+        if (p2 != null)
+            p2.display();
+        else
+            System.out.println("Product Not Found");
+    }
+}
